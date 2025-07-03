690eff1c8ca8604685a7c3a7641b05062b6ac66c
Israel.Alexandre <israel4lexandre@gmail.com>
2025-07-03 12:32:53 -0300

    Entrega do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Testes com as classes Nokia e Iphone 
+
+Console.WriteLine("Bem vindo ao sistema de Teste de Smartphones!\n");
+
+Console.Clear();
+Console.WriteLine("Smartphone Nokia Lumia\n");
+Smartphone nokia = new Nokia("11999999999", "Lumia 920", "123456789", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("MSN Messenger");
+
+Console.WriteLine("\nSmartphone iPhone\n");
+Smartphone iphone = new Iphone("11988888888", "iPhone 12", "987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
